2f5a84dcaadf90dfb6ea3b326ca17ddf83da298e
varti2023 <varti.pant@gmail.com>
2023-07-31 19:07:30 -0500

    changed fuelLevel and CargoMass check messages

--- scriptHelper.js
+++ scriptHelper.js
@@ -26,11 +26,11 @@ function validateInput(testInput) {
     }
     else if (!isNaN(testInput))
     {
-        return "Is a number";
+        return "Is a Number";
     }
     else if (isNaN(testInput))
     {
-        return "Not a number";
+        return "Not a Number";
     }
  }
 
@@ -56,15 +56,15 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
 
         if(fuelLevel < 10000 || cargoMass > 10000){
             launchStatus.innerHTML  = "Shuttle Not Ready for Launch";
-            launchStatus.style.color = "red";
-            fuelStatus.innerHTML  = "Fuel level too low for launch";
+            launchStatus.style.color = "rgb(199, 37, 78)";
+            fuelStatus.innerHTML  = "Fuel level high enough for launch";
             cargoStatus.innerHTML  = "Cargo mass too heavy for launch";
 
         }else if(fuelLevel < 10000){
             launchStatus.innerHTML  = "Shuttle Not Ready for Launch";
             fuelStatus.innerHTML  = "Fuel level too low for launch";
             cargoStatus.innerHTML  = "Cargo mass low enough for launch";
-            launchStatus.style.color = "red";
+            launchStatus.style.color = "rgb(199, 37, 78)";
       
         }else if(cargoMass > 10000){
             launchStatus.innerHTML  = "Shuttle Not Ready for Launch";
@@ -75,7 +75,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
         }else{
             fuelStatus.innerHTML = "Fuel level high enough for launch"
             cargoStatus.innerHTML = "Cargo mass low enough for launch"
-            launchStatus.innerHTML  = "Shuttle Ready for Launch";
+            launchStatus.innerHTML  = "Shuttle is Ready for Launch";
             launchStatus.style.color = "#419F6A";
 
         }
